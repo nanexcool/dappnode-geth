495978848581df69094af12c7989ae32a6e223fe
Luka Gulin <lg@shardlabs.io>
2023-10-11 15:40:37 +0200

    Bump upstream and change state schema

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,19 +31,4 @@ esac
 JWT=$(cat $JWT_PATH)
 curl -X POST "http://my.dappnode/data-send?key=jwt&data=${JWT}"
 
-exec geth \
-  --http --http.addr 0.0.0.0 \
-  --http.corsdomain "*" \
-  --http.vhosts "*" \
-  --syncmode ${SYNCMODE:-snap} \
-  --ws \
-  --ws.origins "*" \
-  --ws.addr 0.0.0.0 \
-  --port ${P2P_PORT} \
-  --metrics \
-  --metrics.addr 0.0.0.0 \
-  --authrpc.addr 0.0.0.0 \
-  --authrpc.port 8551 \
-  --authrpc.vhosts "*" \
-  --authrpc.jwtsecret ${JWT_PATH} \
-  $EXTRA_OPTION
+exec geth --authrpc.jwtsecret ${JWT_PATH} $EXTRA_OPTION
